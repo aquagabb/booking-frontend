fe4c0cc0f8c6da859ff396afbe440b7c6c79269b
aquagabb <gabb16y.bg@gmail.com>
2026-06-27 01:46:24 +0300

    yarn update + removed libraries

--- frontend_client/eslint.config.js
+++ frontend_client/eslint.config.js
@@ -1,5 +1,6 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import reactCompiler from 'eslint-plugin-react-compiler'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
@@ -14,11 +15,13 @@ export default tseslint.config(
       globals: globals.browser,
     },
     plugins: {
+      'react-compiler': reactCompiler,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      'react-compiler/react-compiler': 'error',
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
